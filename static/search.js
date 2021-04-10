a0337dabd606dc7cd6b83600bd7fb8702229a4ef
ogradybj <ogradybj@gmail.com>
2021-04-10 17:34:33 -0500

    initial commit

---
+++ static/search.js
@@ -0,0 +1,7 @@
+fuction view (data){
+    var canvas = document.getElementById("paint");
+    var ctx = canvas.getContext("2d");
+    var newImg = document.createElement("img");
+    newImg.src = data;
+    document.body.appendChile(newImg);
+}
